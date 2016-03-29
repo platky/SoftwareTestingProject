b9b7377d2491f6577c83eb3a8bbf9ba9fd49583d
platky <platky1@gmail.com>
2016-03-28 21:42:03 -0400

    Parser fixed

--- pi/partA/Parser.java
+++ pi/partA/Parser.java
@@ -17,21 +17,33 @@ public class Parser {
 		
 		CallGraph cg = new CallGraph();
 		Node node = null;
+		
+		Boolean inNode = false;
 
 		while (line != null) {
 			// TODO: Should this be reworked into a rigorous regex matching?
 			if (line.startsWith("Call graph node for function")) {
+				//System.out.println("starter");
 				node = new Node(retrieveFunctionName(line));
+				inNode=true;
 			} else if (line.startsWith("  CS<0x")) {
-				assert(node != null);
-				String child = retrieveFunctionName(line);
-				if (!node.containsChild(child)) {
-					node.addChild(retrieveFunctionName(line));	// add child (avoid duplicate)
+				//System.out.println("address add");
+				//can be "calls function" or "calls node"
+				if(inNode){
+					if(!line.contains("calls external node")){
+						assert(node != null);
+						String child = retrieveFunctionName(line);
+						if (!node.containsChild(child)) {
+							node.addChild(retrieveFunctionName(line));	// add child (avoid duplicate)
+						}
+					}
 				}
 			} else {
+				inNode=false;
 				// No more information regarding the current working node
 				// add it to the call graph model.
 				if (node != null) {
+				//System.out.println("null function");
 					cg.addNode(node);
 					node = null;
 				}
@@ -48,8 +60,9 @@ public class Parser {
 	
 	// returns the function name surrounded by quotation marks (note: not apostrophes)
 	public String retrieveFunctionName(String line) {
-		int firstIndex = line.indexOf('‘');
-		int lastIndex = line.lastIndexOf('’');
+		int firstIndex = line.indexOf('\'');
+		int lastIndex = line.lastIndexOf('\'');
+		System.out.println("starts at "+firstIndex+" ends at "+lastIndex);
 		
 		return line.substring(firstIndex + 1, lastIndex);
 	}
